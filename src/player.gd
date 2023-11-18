0bc3cd3840268c7c89a5d33aff13d51a47dfbd2e
Alice Figueiredo <alicefigueiredo837@gmail.com>
2023-11-17 23:21:54 -0300

    Add player character

---
+++ src/player.gd
@@ -0,0 +1,17 @@
+extends CharacterBody2D
+
+@export var jump_velocity = 2500.0
+
+# Get the gravity from the project settings to be synced with RigidBody nodes.
+var gravity: int = ProjectSettings.get_setting('physics/2d/default_gravity')
+
+func _physics_process(delta: float) -> void:
+	# Add the gravity.
+	if not is_on_floor():
+		velocity.y += gravity * delta
+
+	# Handle Jump.
+	if Input.is_action_pressed('ui_accept'):
+		velocity.y -= jump_velocity * delta
+
+	move_and_slide()
