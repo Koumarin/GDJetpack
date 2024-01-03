b7d54d0f303f9a920e08dcb712a0d59447c3c548
Alice Figueiredo <alicefigueiredo837@gmail.com>
2024-01-03 15:19:57 -0300

    Add signal when obstacles are hit by the player

--- src/obstacle.gd
+++ src/obstacle.gd
@@ -1,8 +1,18 @@
-extends AnimatableBody2D
+extends Area2D
+
+signal player_hit
 
 @export var speed = 250
+
+func _ready() -> void:
+	body_entered.connect(_on_player_hit)
 
 func _physics_process(delta: float) -> void:
 	var velocity := Vector2(-speed, 0)
 	
-	move_and_collide(delta * velocity)
+	position += delta * velocity
+
+func _on_player_hit(_body) -> void:
+	print('Player hit!')
+	queue_free()
+	emit_signal('player_hit')
